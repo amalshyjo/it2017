01b261bdf395bc3d95d0949d151708732e4615fa
amal shyjo <amalshyjo@gmail.com>
2020-05-14 20:45:56 +0530

    version 0.0.8

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,17 @@
-function passWord() {
- 
-    var pass1 = prompt('Please Enter Your Password',' ');
-  
-    if (pass1.toLowerCase() == "letmein") {
-    window.open('contact.html');
-    }
-     else {
-        window.open('login.html');
-    }
-    }
+
+// PASSWORD PROTECTION SCRIPT
+
+function TheLogin() {
+
+var password = 'letmein';
+var x = document.getElementById("wrongpass");
+var y = document.getElementById("pass");
+
+if (this.document.login.pass.value == password) {
+  top.location.href="contact.html";
+}
+else if (this.document.login.pass.value !== password){
+    x.style.display = "block";
+    y.style.display = "none";
+}
+}
