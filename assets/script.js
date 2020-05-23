496ba8ddc19d465a276c592c9e0985013c20ffc1
amal shyjo <amalshyjo@gmail.com>
2020-05-24 04:14:32 +0530

    Version 1.0.2

--- assets/script.js
+++ assets/script.js
@@ -15,11 +15,3 @@ else if (this.document.login.pass.value !== password){
     y.style.display = "none";
 }
 }
-function form() {
-  var x = document.getElementById("form");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
